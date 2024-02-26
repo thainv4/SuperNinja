56444edf920e649f19422e2dd478b5bf75e43c41
DungX <126141756+dungnot@users.noreply.github.com>
2024-02-26 20:27:04 +0700

    Add Items

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inventory.Items;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,15 +7,35 @@ public class Player : MonoBehaviour
     [Header("Config")]
     [SerializeField] private PlayerStats stats;
 
+    [Header("Test")]
+    public ItemHealthPotion HealthPotion;
+    public ItemManaPotion ManaPotion;
     public PlayerStats Stats => stats;
 
     private PlayerAnimations animations;
     public PlayerMana PlayerMana { get; private set; }
-
+    public PlayerHealth PlayerHealth { get; private set; }
     private void Awake()
     {
         animations = GetComponent<PlayerAnimations>();
+        PlayerHealth = GetComponent<PlayerHealth>();
         PlayerMana = GetComponent<PlayerMana>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T)) 
+        {
+            if(HealthPotion.UseItem()) 
+            {
+                Debug.Log("Using Health Potion");
+            }
+
+            if (ManaPotion.UseItem())
+            {
+                Debug.Log("Using Mana Potion");
+            }
+        }
     }
 
     public void ResetPlayer()
